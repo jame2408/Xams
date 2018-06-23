3f74f0b393a151cdf019baac1cd9b5ecb00171e8
JamesWang <jame2408@gmail.com>
2018-06-23 15:26:16 +0800

    初始版本

---
+++ Lab1_Xams/SourceCode/Holiday.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1_Xams.SourceCode
+{
+    public class Holiday
+    {
+        public string IsTodayXmas()
+        {
+            if (DateTime.Now.Year == 12 && DateTime.Now.Month == 25)
+            {
+                return "Happy Xmas!";
+            }
+
+            return "Today is not Xmas";
+        }
+    }
+}
---
+++ Lab1_Xams/SourceCode/TestHoliday.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Lab1_Xams.SourceCode
+{
+    [TestClass]
+    public class TestHoliday
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
