ddc8db00083590718431489491ea2c2964777e5a
JamesWang <jame2408@gmail.com>
2018-06-23 16:42:13 +0800

    [ 橫切面關注點分離 ] 利用橫切面關注點分離隔絕相依。

---
+++ Lab1_Xams/3_AOP/Holiday.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Lab1_Xams._3_AOP
+{
+    public class Holiday
+    {
+        public string IsTodayXmas()
+        {
+            if (SystemTime.Now.Month == 12 && SystemTime.Now.Day == 25)
+            {
+                return "Happy Xmas!";
+            }
+
+            return "Today is not Xmas";
+        }
+    }
+
+    public class SystemTime
+    {
+        private static DateTime _date;
+
+        public static void Set(DateTime custom) => _date = custom;
+
+        public static void ReSet() => _date = DateTime.MinValue;
+
+        public static DateTime Now
+        {
+            get
+            {
+                if (_date != DateTime.MinValue)
+                {
+                    return _date;
+                }
+                return DateTime.Now;
+            }
+        }
+    }
+}
---
+++ Lab1_Xams/3_AOP/TestHoliday.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Lab1_Xams._3_AOP
+{
+    [TestClass]
+    public class TestHoliday
+    {
+        [TestInitialize]
+        public void BeforeEachTestMethods()
+        {
+            SystemTime.ReSet();
+        }
+
+        [TestMethod]
+        public void Today_Is_Xmas()
+        {
+            SystemTime.Set(new DateTime(2018, 12, 25));
+            TodayShouldBe("Happy Xmas!");
+        }
+
+        [TestMethod]
+        public void Today_Is_Not_Xmas()
+        {
+            SystemTime.Set(new DateTime(2018, 12, 24));
+            TodayShouldBe("Today is not Xmas");
+        }
+
+        private static void TodayShouldBe(string expected)
+        {
+            var target = new Holiday();
+            var actual = target.IsTodayXmas();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCleanup]
+        public void AfterEachTestMethods()
+        {
+            SystemTime.ReSet();
+        }
+    }
+}
