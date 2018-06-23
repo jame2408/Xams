70f6397f75cb94970efe9f2fccbb86438ee9133d
JamesWang <jame2408@gmail.com>
2018-06-23 16:05:48 +0800

    [ Extract & Override ] 利用擷取與覆寫隔離相依。

---
+++ Lab1_Xams/2_Extract And Override/Holiday.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Lab1_Xams._2_Extract_And_Override
+{
+    public class Holiday
+    {
+        public string IsTodayXmas()
+        {
+            if (Today().Month == 12 && Today().Day == 25)
+            {
+                return "Happy Xmas!";
+            }
+
+            return "Today is not Xmas";
+        }
+
+        protected virtual DateTime Today()
+        {
+            return DateTime.Now;
+        }
+    }
+}
---
+++ Lab1_Xams/2_Extract And Override/TestHoliday.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Lab1_Xams._2_Extract_And_Override
+{
+    [TestClass]
+    public class TestHoliday
+    {
+        [TestMethod]
+        public void Today_Is_Xams()
+        {
+            var target = new HolidayStub { SetDateTime = new DateTime(2018, 12, 25) };
+            var actual = target.IsTodayXmas();
+            Assert.AreEqual("Happy Xmas!", actual);
+        }
+
+        [TestMethod]
+        public void Today_Is_Not_Xams()
+        {
+            var target = new HolidayStub { SetDateTime = new DateTime(2018, 12, 24) };
+            var actual = target.IsTodayXmas();
+            Assert.AreEqual("Today is not Xmas", actual);
+        }
+    }
+
+    public class HolidayStub : Holiday
+    {
+        private DateTime _dateTime;
+
+        public DateTime SetDateTime
+        {
+            set => _dateTime = value;
+        }
+
+        protected override DateTime Today()
+        {
+            return _dateTime;
+        }
+    }
+}
